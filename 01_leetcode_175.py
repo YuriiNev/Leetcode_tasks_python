b5d4761231b87f1d82b9b8fb2521ac6a8063f984
YuriiNev <nevskii.u@gmail.com>
2022-01-12 14:16:28 +1100

    Leetcode task 175 (program 01) is completed

--- 01_leetcode_175.py
+++ 01_leetcode_175.py
@@ -102,13 +102,14 @@ person_cnt = [['personId', 'INTEGER'],
               ['lastName', 'TEXT'],
               ['firstName', 'TEXT']]
 
-# a separate list of column names (not necessary: may be optimized)
+# a separate list of column names
 person_cn = []
 for cname in person_cnt:
     person_cn.append(cname[0])
 # end for cname in person_cnt:
 
 # Person_175 table content
+#         personId, sirname, name
 person_table = [[1, "Wang", "Allen"],
                 [2, "Alice", "Bob"],
                 [3, "Bond", "James"],
@@ -122,13 +123,14 @@ address_cnt = [['addressId', 'INTEGER'],
                ['city', 'TEXT'],
                ['state', 'TEXT']]
 
-# a separate list of column names (not necessary: may be optimized)
+# a separate list of column names
 address_cn = []
 for cname in address_cnt:
     address_cn.append(cname[0])
 # end for cname in person_cnt:
 
 # Address_175 table content
+#    addressId, personId, city,      state
 address_table = [[1, 2, 'Melbourne', 'Victoria'],
                  [2, 3, 'Sydney', 'NSW'],
                  [3, 7, 'New York City', 'New York'],
@@ -214,41 +216,116 @@ curo = outp.cursor()
 curo.execute("DROP TABLE IF EXISTS " + t_names[2])
 
 SQL_st = sqf.SQL_CREATE_Table(t_names[2])  # row_id column created automatically
-cur.execute(SQL_st)
+curo.execute(SQL_st)
 
+pc_cnt = person_cnt  # creating the list of column names and types
+for val in address_cnt:
+    if val not in pc_cnt:
+        pc_cnt.append(val)
+    # end if val not in pc_cnt
+# end for val in address_cnt
+pc_cnt.remove(pc_cnt[0])  # now has new indexation
+pc_cnt.remove(pc_cnt[2])
+
+""" Initiating creation of the structure of t_names[2] = Person_Address_175"""
+for column_n_t in pc_cnt:  # [0] - column name; [1] - column type
+    SQL_st = sqf.SQL_ADD_Column(t_names[2],
+                                column_n_t[0],
+                                column_n_t[1])
+    curo.execute(SQL_st)
+# end for column_n_t in person_cnt:
+
+# a separate list of column names
+pc_cn = []
+for cname in pc_cnt:
+    pc_cn.append(cname[0])
+# end for cname in person_cnt:
 """ output table has been created ================"""
 
 """ merging columns with personId =============="""
 
 curi = inp.cursor()
 
-SQL_st = "SELECT " + cc + " FROM " + t_names[0] + " ORDER BY " + cc + " ASC"
-cc_t0 = curi.execute(SQL_st).fetchall()
+# receing the values in the rows with personId in each table
+t0_cc = []
+SQL_st = "SELECT " + cc + " FROM " + t_names[0]
+for val in curi.execute(SQL_st).fetchall():
+    t0_cc.append(val)
+# end for
 
-SQL_st = "SELECT " + cc + " FROM " + t_names[1] + " ORDER BY " + cc + " ASC"
-cc_t1 = curi.execute(SQL_st).fetchall()
+t1_cc = []
+SQL_st = "SELECT " + cc + " FROM " + t_names[1]
+for val in curi.execute(SQL_st).fetchall():
+    t1_cc.append(val)
+# end for
 
-cc_me = []  # common column merged
-for i, value in enumerate(cc_t0):
-    cc_me.append(value[0])
+"""
+1) check personId in the 1 table,
+2) take the values of the corresponding row from the 1 table
+3) check if the second table has the same personId value
+3.5) delete the value from the list for preventing a double usage
+4) take the values of the corresponding row from the 2 table
+5) add values from 4) to the list with values from 2)
+6) write the list into the table 3
+"""
+for val in t0_cc:
+    row_val = []
+    SQL_st = "SELECT * FROM " + t_names[0] + " WHERE " + cc + " = " + str(val[0])
+    cc_t0 = curi.execute(SQL_st).fetchone()
+    # adding into the resulting row all values except for the first 2
+    for i in range(len(cc_t0)-2):
+        row_val.append(cc_t0[i+2])
+    # end for
 
-k = 0
-equality = False
-k_max = len(cc_me)
-for i, value in enumerate(cc_t1):
-    if value[0] < cc_me[k] and equality is False:
-        cc_me.append(value[0])
+    if val in t1_cc:
+        SQL_st = "SELECT * FROM " + t_names[1] + " WHERE " + cc + " = " + str(val[0])
+        cc_t1 = curi.execute(SQL_st).fetchone()
+        # adding into the resulting row all values except for the first 3
+        for j in range(len(cc_t1)-3):
+            row_val.append(cc_t1[j+3])
+        # end for
+        t1_cc.remove(val)
+    # end if val in t1_cc:
     else:
-        while cc_me[k] < value[0] and k < k_max:
-            k += 1
-            equality = False
-        if value[0] == cc_me[k]:
-            equality = True
-    # end if value[0] < cc_me[k]:
-# end for i, value in enumerate(cc_t1)
+        for i in range(len(address_cn)-2):  # -2 (not -3) due to automatical column
+            row_val.append('Null')
+        # end for
+    # end else
+    # writing the row into the output table
+    SQL_st = sqf.SQL_INSERT_INTO(
+      t_names[2],
+      pc_cn,
+      row_val)
+    curo.execute(SQL_st)
+    # end for i in range(len(person_table)):
+
+"""
+1) check the remaining list of personalId -> t1_cc,
+2) for each value in the list take the corresponding row
+3) create the row for the new table with absent columns filled with None
+4) write the row into the table
+"""
+
+for val in t1_cc:  # only elemens different from t0_cc remains
+    row_val = []
+    for i in range(len(person_cn)-1):  # -1 (not -2) due to automatical column
+        row_val.append('Null')
+    # end for
+    SQL_st = "SELECT * FROM " + t_names[1] + " WHERE " + cc + " = " + str(val[0])
+    cc_t1 = curi.execute(SQL_st).fetchone()
+    # adding into the resulting row all values except for the first 3
+    for j in range(len(cc_t1)-3):
+        row_val.append(cc_t1[j+3])
+    # end for
+    # writing the row into the output table
+    SQL_st = sqf.SQL_INSERT_INTO(
+      t_names[2],
+      pc_cn,
+      row_val)
+    curo.execute(SQL_st)
 
 
 """ columns with personId have been merged into cc_me list =============="""
-
+outp.commit()  # commiting changes into output DB, do not commit into input DB
 outp.close()  # outp = sqlite3.connect(o_p)
 inp.close()  # inp = sqlite3.connect(i_p)
--- test1.py
+++ test1.py
@@ -3,21 +3,26 @@ b = (1, 4, 5, 7, 12, 21, 22, 23, 25, 27)
 
 c_corr = [1, 2, 3, 4, 5, 7, 9, 12, 14, 21, 22, 23, 25, 27]
 c = []
+
+
+""" merging 2 sorted lists using m+n operations """
+# adding first list to the resulting array
 for i in range(len(a)):
     c.append(a[i])
-
 k = 0
-
 k_max = len(c)
+
 for i, value in enumerate(b):
+    # go through c[k] while its elements lesser than the values in merging array
     while c[k] < value and k < k_max:
         k += 1
         ck_val = c[k]
     # end while
-    if c[k] > value:
+    # at this point c[k] can be equal or bigger than value
+    if c[k] > value:  # c[k] is not equal, so adding value to c[k]
         c.append(value)
     # end if c[k] > value
-    if a[k_max-1] < value:
+    if a[k_max-1] < value:  # if there are values more than the giggest element in c
         c.append(value)
     # end if a[k_max-1] < value:
 # end for i, value in enumerate(cc_t1)
