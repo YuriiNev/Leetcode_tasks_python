75b84f655d24f0ee7de8fae79777dfe5f0415cf7
YuriiNev <nevskii.u@gmail.com>
2022-01-24 10:26:56 +1100

    leetcode.com Task 176. Second Highest Salary.

--- 02_leetcode_176.py
+++ 02_leetcode_176.py
@@ -10,7 +10,7 @@ id is the primary key column for this table.
 Each row of this table contains information about the salary of an employee.
 
 Write an SQL query to report the second highest salary from the Employee table.
-If there is no second highest salary, the query should report null.
+If there is no second highest salary, the query should report null (none).
 
 The query result format is in the following example.
 
